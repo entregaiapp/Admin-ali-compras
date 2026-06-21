40d41efb94c4d4557ccb9e0354b6bfa4f1d9007c
Fernando Álisson <fernando.alisson@ufpe.br>
2026-06-21 12:14:58 -0300

    Refactor code structure for improved readability and maintainability

--- dist/assets/notificationsService-DYjEJYxh.js
+++ dist/assets/notificationsService-DYjEJYxh.js
@@ -1,4 +1,4 @@
-import{a as O}from"./index-BNR4DfM3.js";const pt=()=>{};var fe={};/**
+import{a as O}from"./index-BP0Rcabn.js";const pt=()=>{};var fe={};/**
  * @license
  * Copyright 2017 Google LLC
  *
