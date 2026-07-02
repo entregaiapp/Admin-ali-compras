60c033ad21956e622e0f80e1bd44113c4680df76
Fernando Álisson <fernando.alisson@ufpe.br>
2026-07-02 12:09:09 -0300

    feat

--- dist/assets/NotificationsPage-DBBDKy5P.js
+++ dist/assets/NotificationsPage-DBBDKy5P.js
@@ -1,4 +1,4 @@
-import{c as w,u as ee,r,m as O,j as a,S as ae,B as te,b as se,f as ne}from"./index-CZg_fvVw.js";import{fetchNotifications as re,fetchCampaigns as ie,enableAdminPush as le,readNotification as $,createCampaign as oe}from"./notificationsService-C5TtKyZM.js";/**
+import{c as w,u as ee,r,m as O,j as a,S as ae,B as te,b as se,f as ne}from"./index-Bf0u9PL-.js";import{fetchNotifications as re,fetchCampaigns as ie,enableAdminPush as le,readNotification as $,createCampaign as oe}from"./notificationsService-QEboYe8v.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/notificationsService-QEboYe8v.js
+++ dist/assets/notificationsService-QEboYe8v.js
@@ -1,4 +1,4 @@
-import{a as O}from"./index-CZg_fvVw.js";const pt=()=>{};var fe={};/**
+import{a as O}from"./index-Bf0u9PL-.js";const pt=()=>{};var fe={};/**
  * @license
  * Copyright 2017 Google LLC
  *
